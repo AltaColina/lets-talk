ee7cb5dcfdcf1d87160d6eeff3e3f7a9e68f0b9b
sadicangel <sadicangel@hotmail.com>
2022-06-18 01:43:24 +0100

    Add project files.

---
+++ LetsTalk/Services/LetsTalkService.cs
@@ -0,0 +1,53 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Threading.Tasks.Dataflow;
+
+namespace LetsTalk.Services;
+
+public class LetsTalkService : LetsTalk.LetsTalkBase
+{
+    private static readonly Person Server = new() { Username = "Server" };
+    private static readonly Task<Empty> EmptyTaskResponse = Task.FromResult(new Empty());
+    private static readonly ConcurrentDictionary<string, Person> LoggedUsers = new();
+    private static readonly BufferBlock<Message> MessageBuffer = new(new DataflowBlockOptions { EnsureOrdered = true });
+    private static readonly ConcurrentDictionary<string, IServerStreamWriter<Message>> Subscriptions = new();
+
+    public override Task<RegisterResponse> Register(RegisterRequest request, ServerCallContext context)
+    {
+        var person = new Person { Username = request.Username };
+        if (request.Username != Server.Username && LoggedUsers.TryAdd(request.Username, person))
+            return Task.FromResult(new RegisterResponse { Person = person });
+        else
+            return Task.FromResult(new RegisterResponse { Person = null });
+    }
+
+    public override async Task Join(Person request, IServerStreamWriter<Message> responseStream, ServerCallContext context)
+    {
+        var username = request.Username;
+        Subscriptions.TryAdd(username, responseStream);
+
+        MessageBuffer.Post(new Message { Person = Server, Text = $"{request.Username} has join the channel." });
+
+        while (Subscriptions.ContainsKey(username))
+        {
+            var message = await MessageBuffer.ReceiveAsync();
+            await Task.WhenAll(Subscriptions.Values.Select(s => s.WriteAsync(message)));
+        }
+    }
+
+    public override Task<Empty> Send(Message request, ServerCallContext context)
+    {
+        MessageBuffer.Post(request);
+        return EmptyTaskResponse;
+    }
+
+    public override Task<Empty> Leave(Person request, ServerCallContext context)
+    {
+        Subscriptions.TryRemove(request.Username, out _);
+        LoggedUsers.TryRemove(request.Username, out _);
+        MessageBuffer.Post(new Message { Person = Server, Text = $"{request.Username} has left the channel." });
+        return EmptyTaskResponse;
+    }
+}
---
+++ LetsTask.Console/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Grpc.Net.Client;
+using LetsTalk;
+using LetsTalkClient = LetsTalk.LetsTalk.LetsTalkClient;
+
+var options = new GrpcChannelOptions
+{
+    HttpHandler = new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+    }
+};
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5219", options);
+
+var client = new LetsTalkClient(channel);
+
+// Register.
+var person = default(Person);
+while (person is null)
+{
+    Console.WriteLine("Please enter a unique username.");
+    var username = Console.ReadLine();
+    var registerResponse = await client.RegisterAsync(new RegisterRequest { Username = username });
+    person = registerResponse.Person;
+    if(person is null)
+        Console.WriteLine($"User '{username}' already exists.");
+}
+
+Task.Run(async () =>
+{
+    var call = client.Join(person);
+    while (await call.ResponseStream.MoveNext(CancellationToken.None))
+    {
+        var message = call.ResponseStream.Current;
+        Console.WriteLine($"{message.Person.Username}: {message.Text}");
+    }
+})
+    .ConfigureAwait(false)
+    .GetAwaiter();
+
+while (Console.ReadLine() is string line && line != "exit")
+    await client.SendAsync(new Message { Person = person, Text = line });
+
+await client.LeaveAsync(person);
