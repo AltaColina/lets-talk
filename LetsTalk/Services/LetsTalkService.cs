a5355e465caf719fb148a515bfe9cf58b34a5107
sadicangel <sadicangel@hotmail.com>
2022-06-20 18:51:43 +0100

    Adds support for multiple chats.

--- LetsTalk/Services/LetsTalkService.cs
+++ LetsTalk/Services/LetsTalkService.cs
@@ -8,46 +8,147 @@ namespace LetsTalk.Services;
 
 public class LetsTalkService : LetsTalk.LetsTalkBase
 {
-    private static readonly Person Server = new() { Username = "Server" };
+    private static readonly User Server = new() { Username = "Server" };
     private static readonly Task<Empty> EmptyTaskResponse = Task.FromResult(new Empty());
-    private static readonly ConcurrentDictionary<string, Person> LoggedUsers = new();
-    private static readonly BufferBlock<Message> MessageBuffer = new(new DataflowBlockOptions { EnsureOrdered = true });
-    private static readonly ConcurrentDictionary<string, IServerStreamWriter<Message>> Subscriptions = new();
+    private static readonly ConcurrentDictionary<string, User> RegisteredUsers = new();
+    private static readonly ConcurrentDictionary<string, User> LoggedUsers = new();
+    private static readonly ConcurrentDictionary<string, Chat> Chats = new();
+    private static readonly ConcurrentDictionary<string, ConcurrentDictionary<string, IServerStreamWriter<Message>>> Subscriptions = new();
+    private static readonly ConcurrentDictionary<string, BufferBlock<Message>> MessageBuffers = new();
+
+    static LetsTalkService()
+    {
+        var chat = new Chat { Id = Guid.NewGuid().ToString(), Name = "General" };
+        if (Chats.TryAdd(chat.Id, chat))
+        {
+            Subscriptions.TryAdd(chat.Id, new ConcurrentDictionary<string, IServerStreamWriter<Message>>());
+            MessageBuffers.TryAdd(chat.Id, new BufferBlock<Message>(new DataflowBlockOptions { EnsureOrdered = true }));
+        }
+    }
+
+    private static void ValidateUser(User user)
+    {
+        if (!LoggedUsers.ContainsKey(user.Id))
+            throw new RpcException(new Status(StatusCode.Unauthenticated, "User is not logged in"));
+    }
+
+    private static void ValidateChannel(Chat chat)
+    {
+        if (!Subscriptions.ContainsKey(chat.Id))
+            throw new RpcException(new Status(StatusCode.NotFound, "Chat does not exist"));
+    }
 
     public override Task<RegisterResponse> Register(RegisterRequest request, ServerCallContext context)
     {
-        var person = new Person { Username = request.Username };
-        if (request.Username != Server.Username && LoggedUsers.TryAdd(request.Username, person))
-            return Task.FromResult(new RegisterResponse { Person = person });
-        else
-            return Task.FromResult(new RegisterResponse { Person = null });
+        // TODO: Validate username here.
+        var user = new User { Id = Guid.NewGuid().ToString(), Username = request.Username };
+        if (!RegisteredUsers.TryAdd(user.Id, user))
+            throw new RpcException(new Status(StatusCode.ResourceExhausted, "Could not register user"));
+        return Task.FromResult(new RegisterResponse { User = user });
     }
 
-    public override async Task Join(Person request, IServerStreamWriter<Message> responseStream, ServerCallContext context)
+    public override Task<Empty> Login(LoginRequest request, ServerCallContext context)
     {
-        var username = request.Username;
-        Subscriptions.TryAdd(username, responseStream);
+        if (!RegisteredUsers.ContainsKey(request.User.Id))
+            throw new RpcException(new Status(StatusCode.Unauthenticated, "User is not registered"));
+        
+        var userId = request.User.Id;
+        if (LoggedUsers.TryAdd(userId, request.User))
+            return EmptyTaskResponse;
 
-        MessageBuffer.Post(new Message { Person = Server, Text = $"{request.Username} has join the channel." });
+        if (LoggedUsers.ContainsKey(userId))
+            throw new RpcException(new Status(StatusCode.AlreadyExists, "Already logged in"));
 
-        while (Subscriptions.ContainsKey(username))
+        throw new RpcException(new Status(StatusCode.Unknown, "Could not login"));
+    }
+
+    public override async Task Join(JoinRequest request, IServerStreamWriter<Message> responseStream, ServerCallContext context)
+    {
+        ValidateUser(request.User);
+        ValidateChannel(request.Chat);
+
+        var channelId = request.Chat.Id;
+        var subscription = Subscriptions[channelId];
+
+        var userId = request.User.Id;
+        subscription.TryAdd(userId, responseStream);
+
+        var messageBuffer = MessageBuffers[channelId];
+        messageBuffer.Post(new Message { User = Server, Text = $"{request.User.Username}#{userId} has joined channel '{request.Chat.Name}'." });
+
+        while (subscription.ContainsKey(userId))
         {
-            var message = await MessageBuffer.ReceiveAsync();
-            await Task.WhenAll(Subscriptions.Values.Select(s => s.WriteAsync(message)));
+            var message = await messageBuffer.ReceiveAsync();
+            await Task.WhenAll(subscription.Values.Select(s => s.WriteAsync(message)));
         }
     }
 
     public override Task<Empty> Send(Message request, ServerCallContext context)
     {
-        MessageBuffer.Post(request);
+        ValidateUser(request.User);
+        ValidateChannel(request.Chat);
+        var messageBuffer = MessageBuffers[request.Chat.Id];
+        messageBuffer.Post(request);
         return EmptyTaskResponse;
     }
 
-    public override Task<Empty> Leave(Person request, ServerCallContext context)
+    public override Task<Empty> Leave(LeaveRequest request, ServerCallContext context)
     {
-        Subscriptions.TryRemove(request.Username, out _);
-        LoggedUsers.TryRemove(request.Username, out _);
-        MessageBuffer.Post(new Message { Person = Server, Text = $"{request.Username} has left the channel." });
+        ValidateUser(request.User);
+        ValidateChannel(request.Chat);
+        var chatId = request.Chat.Id;
+        var subscription = Subscriptions[chatId];
+        var userId = request.User.Id;
+        subscription.TryRemove(userId, out _);
+        LoggedUsers.TryRemove(userId, out _);
+        MessageBuffers[chatId].Post(new Message { User = Server, Text = $"{request.User.Username}#{userId} has left channel '{request.Chat.Name}'." });
+        return EmptyTaskResponse;
+    }
+
+    public override Task<GetChatResponse> GetChat(GetChatRequest request, ServerCallContext context)
+    {
+        var response = new GetChatResponse();
+        switch (request.FilterCase)
+        {
+            case GetChatRequest.FilterOneofCase.ChatId:
+                response.Chats.Add(Chats[request.ChatId]);
+                break;
+            case GetChatRequest.FilterOneofCase.ChatName:
+                response.Chats.AddRange(Chats.Values.Where(chat => chat.Name == request.ChatName));
+                break;
+            default:
+                response.Chats.AddRange(Chats.Values);
+                break;
+        }
+
+        if (response.Chats.Count == 0)
+            throw new RpcException(new Status(StatusCode.NotFound, "No chats"));
+
+        return Task.FromResult(response);
+    }
+
+    public override Task<PostChatResponse> PostChat(PostChatRequest request, ServerCallContext context)
+    {
+        var chat = new Chat { Id = Guid.NewGuid().ToString(), Name = request.Name };
+        if (!Chats.TryAdd(chat.Id, chat))
+            throw new RpcException(new Status(StatusCode.Unknown, "Could not add chat"));
+        
+        Subscriptions.TryAdd(chat.Id, new ConcurrentDictionary<string, IServerStreamWriter<Message>>());
+        MessageBuffers.TryAdd(chat.Id, new BufferBlock<Message>(new DataflowBlockOptions { EnsureOrdered = true }));
+        return Task.FromResult(new PostChatResponse { Chat = chat });
+    }
+
+    public override Task<Empty> DeleteChat(DeleteChatRequest request, ServerCallContext context)
+    {
+        var chatId = request.Chat.Id;
+        if (!Chats.TryRemove(chatId, out _))
+            throw new RpcException(new Status(StatusCode.NotFound, $"Chat '{request.Chat.Id}' does not exist"));
+        
+        MessageBuffers[chatId].Complete();
+        MessageBuffers.TryRemove(chatId, out _);
+        Subscriptions[chatId].Clear();
+        Subscriptions.TryRemove(chatId, out _);
+        
         return EmptyTaskResponse;
     }
 }
--- LetsTask.Console/Program.cs
+++ LetsTask.Console/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Grpc.Core;
 using Grpc.Net.Client;
 using LetsTalk;
 using LetsTalkClient = LetsTalk.LetsTalk.LetsTalkClient;
@@ -17,30 +18,58 @@ using var channel = GrpcChannel.ForAddress("http://localhost:5219", options);
 var client = new LetsTalkClient(channel);
 
 // Register.
-var person = default(Person);
-while (person is null)
+var user = default(User);
+while (user is null)
 {
     Console.WriteLine("Please enter a unique username.");
     var username = Console.ReadLine();
-    var registerResponse = await client.RegisterAsync(new RegisterRequest { Username = username });
-    person = registerResponse.Person;
-    if(person is null)
+    try
+    {
+        var registerResponse = await client.RegisterAsync(new RegisterRequest { Username = username });
+        user = registerResponse.User;
+    }
+    catch (RpcException ex) when (ex.StatusCode == StatusCode.AlreadyExists)
+    {
         Console.WriteLine($"User '{username}' already exists.");
+    }
 }
 
+// Login.
+await client.LoginAsync(new LoginRequest { User = user });
+// Add private channel.
+await client.PostChatAsync(new PostChatRequest { Name = $"{user.Username}'s chat" });
+
+// Join a chat.
+var chat = default(Chat);
+while (chat is null)
+{
+    var chats = (await client.GetChatAsync(new GetChatRequest())).Chats;
+    Console.WriteLine("Select a channel to join by typing its number.");
+    for (int i = 0; i < chats.Count; ++i)
+        Console.WriteLine($"{i + 1}: {chats[i].Name}");
+    var input = Console.ReadLine();
+    if (!Int32.TryParse(input, out int number))
+        Console.WriteLine("Input must be a number.");
+    else if ((number - 1) is var index && (index < 0 || index >= chats.Count))
+        Console.WriteLine($"Must be a number between {1} and {chats.Count}.");
+    else
+        chat = chats[index];
+}
 Task.Run(async () =>
 {
-    var call = client.Join(person);
+    var call = client.Join(new JoinRequest { Chat = chat, User = user });
     while (await call.ResponseStream.MoveNext(CancellationToken.None))
     {
         var message = call.ResponseStream.Current;
-        Console.WriteLine($"{message.Person.Username}: {message.Text}");
+        Console.WriteLine($"{message.User.Username}: {message.Text}");
     }
 })
     .ConfigureAwait(false)
     .GetAwaiter();
 
+// Send messages.
 while (Console.ReadLine() is string line && line != "exit")
-    await client.SendAsync(new Message { Person = person, Text = line });
+    await client.SendAsync(new Message { Chat = chat, User = user, Text = line });
 
-await client.LeaveAsync(person);
+// Leave chat.
+await client.LeaveAsync(new LeaveRequest { Chat = chat, User = user });
