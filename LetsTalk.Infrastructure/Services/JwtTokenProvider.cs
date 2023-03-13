5bcbd2f40e81eaa329967b8d6cdc42f1e949f378
sadicangel <sadicangel@hotmail.com>
2023-03-13 11:10:50 +0000

    Removes leftovers.

--- LetsTalk.Infrastructure/Services/JwtTokenProvider.cs
+++ LetsTalk.Infrastructure/Services/JwtTokenProvider.cs
@@ -9,15 +9,13 @@ namespace LetsTalk.Services;
 
 internal sealed class JwtTokenProvider : IJwtTokenProvider
 {
-    private readonly ISymmetricEncryptor _encryptor;
     private readonly SigningCredentials _signingCredentials;
     private readonly JsonWebTokenHandler _tokenHandler;
     private readonly TimeSpan _accessTokenExpireTime;
     private readonly TimeSpan _refreshTokenExpireTime;
 
-    public JwtTokenProvider(ISymmetricEncryptor encryptor, IConfiguration configuration)
+    public JwtTokenProvider(IConfiguration configuration)
     {
-        _encryptor = encryptor;
         var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.GetRequiredSection("SigningKey").Value!));
         _signingCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256Signature);
         _tokenHandler = new JsonWebTokenHandler();
@@ -50,7 +48,6 @@ internal sealed class JwtTokenProvider : IJwtTokenProvider
         expires = issuedAt.Add(_refreshTokenExpireTime);
         var name = identity.Claims.Single(c => c.Type == ClaimTypes.Name).Value;
         var payload = $$"""{"{{JwtRegisteredClaimNames.Name}}":{{name}},"{{JwtRegisteredClaimNames.Nbf}}:{{issuedAt}},"{{JwtRegisteredClaimNames.Exp}}: {{expires}},"{{JwtRegisteredClaimNames.Iss}}: {{issuedAt}}}""";
-        var k = _encryptor.Encrypt(payload);
         return Convert.ToBase64String(Encoding.UTF8.GetBytes(payload));
     }
 
