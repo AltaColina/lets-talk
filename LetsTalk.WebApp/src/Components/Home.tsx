589b1b7e523c5f53421d1eba3b7bb12272aa62dd
bruno.oliveira <bruno.oliveira@albatroz-eng.com>
2023-03-17 12:22:52 +0000

    Added bars to page

--- LetsTalk.WebApp/src/Components/Home.tsx
+++ LetsTalk.WebApp/src/Components/Home.tsx
@@ -3,12 +3,16 @@ import { Suspense } from "react";
 import { wrapPromise } from "../Callbacks/wrap-promise";
 import { hubClient } from "../Services/hub-client";
 import { Lobby } from "./Lobby";
+import { Top } from "./Top";
+import { Footer } from "./Footer";
 
 export const Home = () => {
   const getUserRooms = wrapPromise(hubClient.getUserRooms());
   return (
     <Suspense fallback={<CircularProgress />}>
+      <Top />
       <Lobby getUserRooms={getUserRooms} />
+      <Footer />
     </Suspense>
   );
 }
