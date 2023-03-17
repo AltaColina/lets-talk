9e22cadce2207cc640318ca15b4177398de577b5
bruno.oliveira <bruno.oliveira@albatroz-eng.com>
2023-03-17 12:23:43 +0000

    Improve login visually

--- LetsTalk.WebApp/src/Components/Login.tsx
+++ LetsTalk.WebApp/src/Components/Login.tsx
@@ -1,4 +1,4 @@
-import { TextField } from "@mui/material";
+import { Avatar, Paper, TextField } from "@mui/material";
 import Button from "@mui/material/Button";
 import { Box } from "@mui/system";
 import React, { useState } from "react";
@@ -6,6 +6,7 @@ import { Navigate } from 'react-router-dom';
 import { httpClient } from "../Services/http-client";
 import { hubClient } from "../Services/hub-client";
 import { messenger } from "../Services/messenger";
+import LockIcon from '@mui/icons-material/Lock';
 
 const areValidCredentials = (username?: string, password?: string): boolean => {
   if (!username || !password)
@@ -57,33 +58,54 @@ export const Login = () => {
 }
 
   return (
-    <Box
-      component="form"
-      sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }}
-      noValidate
-      autoComplete="off">
-      {values.isLoggedIn && (<Navigate to="/" replace={true} />)}
-      <div>
-        <TextField
-          required
-          label="Username"
-          type='text'
-          onChange={onUsernameChanged} />
-        <TextField
-          required
-          label="Password"
-          type="password"
-          onChange={onPasswordChanged}
-          onKeyDown={onPasswordKeyDown} />
-      </div>
-      <div>
-        <Button
-          variant="contained"
-          onClick={onLoginClicked}
-          disabled={!values.isValid}>
-          Login
-        </Button>
-      </div>
-    </Box>
+      <Paper
+        sx={{
+          minHeight: '25em',
+          maxHeight: '50em',
+          minWidth: '20em',
+          maxWidth: '25em',
+          margin: '20px auto'
+        }}
+        elevation={10}>
+        <Box
+          component="form"
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            p: 3,
+            gap: 1
+          }}
+          noValidate
+          autoComplete="off">
+          {values.isLoggedIn && (<Navigate to="/" replace={true} />)}
+          <Avatar sx={{ backgroundColor: 'green' }}><LockIcon/></Avatar>
+          <h2>Sign In</h2>
+          <TextField
+            required
+            fullWidth
+            label="Username"
+            type='text'
+            onChange={onUsernameChanged} />
+          <TextField
+            // sx={{ '& label.Mui-focused': { color: 'green' } }}
+            // sx={{ '& .MuiOutlinedInput-root': { '&.Mui-focused fieldset': { borderColor: 'green' } } }}
+            // sx={{ '& .MuiOutlinedInput-root': { '& input:invalid + fieldset': { borderColor: 'red' } } }}
+            required
+            fullWidth
+            label="Password"
+            type="password"
+            onChange={onPasswordChanged}
+            onKeyDown={onPasswordKeyDown} />
+          <Button
+            fullWidth
+            variant="contained"
+            onClick={onLoginClicked}
+            disabled={!values.isValid}>
+            Login
+          </Button>
+        </Box>
+      </Paper>
   );
 }
