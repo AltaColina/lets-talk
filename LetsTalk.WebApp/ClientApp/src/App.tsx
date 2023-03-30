f7227426437abd98466296ab31866886dd9362ee
bruno.oliveira <bruno.oliveira@albatroz-eng.com>
2023-03-31 00:26:49 +0100

    Reorganized routes and layout

--- LetsTalk.WebApp/ClientApp/src/App.tsx
+++ LetsTalk.WebApp/ClientApp/src/App.tsx
@@ -1,37 +1,44 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { SnackbarProvider } from 'notistack';
 import { useEffect, useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 import './App.css';
 import { attachMessageHandlers } from './Callbacks/attach-message-handlers';
-import { Home } from './Components/Home';
 import { Login } from './Components/Login';
 import { PrivateRoutes } from './Components/PrivateRoutes';
 import { messenger } from './Services/messenger';
 import { themeManager } from './Services/theme-manager';
+import Page404 from './Pages/404';
+import Layout from './Pages/Layout';
+import Test from './Pages/Test';
+import { Room } from './Components/Room';
 
 attachMessageHandlers(messenger);
 
 const App = () => {
   const [ theme, setTheme ] = useState(themeManager.theme);
   useEffect(() => messenger.on('ThemeChanged', e => setTheme(e.detail)).dispose);
-  return (
-    <div className="App">
-      <SnackbarProvider anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}>
-        <Router>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          {/* <Themes></Themes> */}
-          <Routes>
-            <Route element={<PrivateRoutes />}>
-              <Route element={<Home />} path="/" />
-            </Route>
-            <Route element={<Login />} path="/login" />
-          </Routes>
-          </ThemeProvider>
-        </Router>
-      </SnackbarProvider>
-    </div>
+    return (
+        <HelmetProvider>
+            <SnackbarProvider anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <Router>
+                        <Routes>
+                            <Route element={<PrivateRoutes />}>
+                                <Route element={<Layout />} path="/">
+                                    <Route element={<Room />} index />
+                                    <Route element={<Test />} path="test" />
+                                </Route>
+                            </Route>
+                                <Route element={<Login />} path="/login" />
+                            <Route element={<Page404 />} path="*" />
+                        </Routes>
+                    </Router>
+                </ThemeProvider>
+            </SnackbarProvider>
+        </HelmetProvider>
   );
 }
 
