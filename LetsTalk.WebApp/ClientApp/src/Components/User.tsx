6cb659b77157ad89b72b1f5efb559def3a92280e
bruno.oliveira <bruno.oliveira@albatroz-eng.com>
2023-04-06 23:59:37 +0100

    Organized user menu options display

--- LetsTalk.WebApp/ClientApp/src/Components/User.tsx
+++ LetsTalk.WebApp/ClientApp/src/Components/User.tsx
@@ -1,28 +1,15 @@
 ﻿import { Box, Divider, Typography, Stack, MenuItem, Avatar, IconButton, Popover, Link } from '@mui/material';
 import { useState } from 'react';
-import { Link as RouterLink } from 'react-router-dom';
 import { useProfileContext } from '../Context/User';
-import { User } from '../Users/user';
+import { Home, Person, Settings, Logout } from '@mui/icons-material';
 
-const MENU_OPTIONS = [
-    {
-        label: 'Home',
-        icon: 'eva:home-fill',
-    },
-    {
-        label: 'Profile',
-        icon: 'eva:person-fill',
-    },
-    {
-        label: 'Settings',
-        icon: 'eva:settings-2-fill',
-    },
-];
+const capitalize = (str: string) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+};
 
 export default function UserPopover() {
     const profileContext = useProfileContext();
     const profile = profileContext.profile;
-    console.log(profile)
     const [open, setOpen] = useState(null);
 
     const handleOpen = (event: any) => {
@@ -32,6 +19,29 @@ export default function UserPopover() {
     const handleClose = () => {
         setOpen(null);
     };
+
+    const MENU_OPTIONS = [
+        {
+            label: 'Home',
+            path: '/',
+            icon: <Home />,
+        },
+        {
+            label: 'Profile',
+            path: '/profile',
+            icon: <Person />,
+        },
+        {
+            label: 'Settings',
+            path: '/settings',
+            icon: <Settings />,
+        },
+        {
+            label: 'Logout',
+            path: profile.logoutUrl,
+            icon: <Logout />,
+        }
+    ];
 
     //const handleCloseLink = (nav?: string) => {
     //    setOpen(null);
@@ -70,7 +80,11 @@ export default function UserPopover() {
             >
                 <Box sx={{ my: 1.5, px: 2.5 }}>
                     <Typography variant="subtitle2" noWrap>
-                        {profileContext.profile.displayName}
+                        {
+                            profileContext.profile.displayName
+                                ? capitalize(profileContext.profile.displayName)
+                                : 'Guest'
+                        }
                     </Typography>
                     <Typography variant="body2" sx={{ color: 'text.secondary' }} noWrap>
                         user.email
@@ -79,24 +93,33 @@ export default function UserPopover() {
 
                 <Divider sx={{ borderStyle: 'dashed' }} />
 
-                <Stack sx={{ p: 1 }}>
+                <Stack
+                    //sx={{ p: 1 }}
+                >
                     {MENU_OPTIONS.map((option) => (
-                        <MenuItem key={option.label} onClick={handleClose}>
-                            {option.label}
+                        <MenuItem
+                            component={Link}
+                            href={option.path}
+                            sx={{ m: 1 }}
+                            onClick={handleClose}
+                        >
+                            { option.icon }
+                            { option.label }
                         </MenuItem>
                     ))}
                 </Stack>
 
-                <Divider sx={{ borderStyle: 'dashed' }} />
+                {/*<Divider sx={{ borderStyle: 'dashed' }} />*/}
 
-                <MenuItem
-                    component={Link}
-                    href={`${profile.logoutUrl}`}
-                    sx={{ m: 1 }}
-                    onClick={handleClose}
-                >
-                    Logout
-                </MenuItem>
+                {/*<MenuItem*/}
+                {/*    component={Link}*/}
+                {/*    href={profile.logoutUrl}*/}
+                {/*    sx={{ m: 1 }}*/}
+                {/*    onClick={handleClose}*/}
+                {/*>*/}
+                {/*    <Logout />*/}
+                {/*    Logout*/}
+                {/*</MenuItem>*/}
             </Popover>
         </>
     );
